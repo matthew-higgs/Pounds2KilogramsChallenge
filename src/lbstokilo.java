89d970c1452b21ee9eb76dddc2fff304d6ef87fb
Chewbacca <higgimat000@warren.k12.in.us>
2024-08-07 13:43:44 -0400

    initial commit

--- src/lbstokilo.java
+++ src/lbstokilo.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class lbstokilo {
+public class lbstokilo {
+    public static void main(String[] args) {
+        double pounds = 200;
+        double kilograms = pounds * 0.45359237;
+        System.out.println(kilograms);
+    }
 }
